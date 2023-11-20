6795c77588ae2f8c6cb76256ed5225bb4aaf85eb
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-20 15:34:57 +0800

    Add files via upload

---
+++ BERKABZNEW/src/main/java/com/mycompany/berkabznew/BERKABZNEW.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.berkabznew;
+
+/**
+ *
+ * @author WS
+ */
+public class BERKABZNEW {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
